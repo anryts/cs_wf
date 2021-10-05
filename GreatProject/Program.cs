2e008606e98d4916f1e9b36d4ed5f410b7696d7f
Andrii Hrytsenko <andrii.hrytsenko02@gmail.com>
2021-10-05 23:06:56 +0300

    Зробив клас Person абстрактним.

--- GreatProject/Program.cs
+++ GreatProject/Program.cs
@@ -19,14 +19,6 @@ namespace GreatProject
         {
             warehouse Bob = new warehouse("Apteka",5,6);
            // Bob.Print();
-            Person Adam = new Person();
-            Person Chief = new Chief();
-            Chief.Print();
-            Item Value1 = new Food("Pizza", 10);
-            Item Value2 = new Electronics();
-            Item Value3 = new Materials();
-            Value2.Print();
-            Value2.Print();
             //Application.EnableVisualStyles();
             //Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new Form1());

c4ed77157ceb2ec95a424afbe6d1bd691398993f
Zertimond <91977198+Zertimond@users.noreply.github.com>
2022-11-21 23:04:26 +0200

    Added unit tests for Person class (Yevhenii Zhmudenko)

---
+++ GreatProject/Person/PersonTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+using Xunit;
+
+namespace GreatProject;
+
+public class PersonTests
+{
+	[Fact]
+	public void Name_Tester_ReturnTester()
+    {
+		//Arrange
+		var sut = new Person("Тестер");
+		string expected = "Тестер";
+
+		//Act
+		string actual = sut.NameOfPerson;
+
+		//Assert
+		Assert.Equal(expected, actual);
+	}
+
+	[Fact]
+	public void CreatePerson_EmptyName_ThrowArgumentNullException()
+    {
+		//Arrange
+		var sut = new Person("");
+
+		//Assert
+		Assert.Throw<ZeroLenghtException>(() => sut._nameOfPerson = "");
+	}
+}
